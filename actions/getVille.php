1caacff6925a68b896f1fa2abfb676c12f678644
FANTINI <francois.fantini@hotmail.fr>
2020-06-01 16:36:36 +0200

    - MODIF MINEURS

--- actions/getVille.php
+++ actions/getVille.php
@@ -2,7 +2,7 @@
 require '../bin/config/database.php';
 //var_dump($_GET);
 $codePostal = $_GET['codePostal'];
-$sqlRequest = $db->prepare("SELECT nomVille FROM connect_life.localisations WHERE codePostal = :codePostal");
+$sqlRequest = $db->prepare("SELECT nomVille FROM localisations WHERE codePostal = :codePostal");
 $sqlRequest->execute([":codePostal" => $codePostal]);
 $resultat = $sqlRequest->fetchAll();
 echo(json_encode($resultat));
--- formulaires/adresse_inputs.php
+++ formulaires/adresse_inputs.php
@@ -1,12 +1,16 @@
 <div class="container">
     <label for="adresse1">Adresse 1 * :</label>
-    <input value="<?php if (isset($_SESSION['adresse1'])){echo $_SESSION['adresse1'];} ?>" type="text" placeholder="1, rue de l'exemple" name="adresse1" id="adresse1">
+    <input value="<?php if (isset($_SESSION['adresse1'])) {
+        echo $_SESSION['adresse1'];
+    } ?>" type="text" placeholder="1, rue de l'exemple" name="adresse1" id="adresse1">
     <p class="messageErreur messageErreurAdresse1"></p>
 </div>
 
 <div class="container">
     <label for="adresse2">Adresse 2 :</label>
-    <input value="<?php if (isset($_SESSION['adresse2'])){echo $_SESSION['adresse2'];} ?>" type="text" placeholder="Etage 2, Appt 28" name="adresse2" id="adresse2">
+    <input value="<?php if (isset($_SESSION['adresse2'])) {
+        echo $_SESSION['adresse2'];
+    } ?>" type="text" placeholder="Etage 2, Appt 28" name="adresse2" id="adresse2">
     <p class="messageErreur messageErreurAdresse2"></p>
 </div>
 
@@ -19,7 +23,7 @@
 <div class="container">
     <label for="ville">Ville * :</label>
     <div class="select-wrapper">
-            <select class="ville" name="ville" id="ville"></select>
-        </div>
+        <select class="ville" name="ville" id="ville"></select>
+    </div>
     <p class="messageErreur messageErreurVille"></p>
 </div>
