434278b0500b8d212da2f89a852043991d752683
FANTINI <francois.fantini@hotmail.fr>
2020-06-02 21:11:17 +0200

    feat/formatString : ajout exporterXml dans routing

--- exporterXmlComplet.php
+++ exporterXmlComplet.php
@@ -1,6 +1,7 @@
 <?php
 require_once './includes/header.php';
 require_once './bin/config/database.php';
+require_once './includes/isClient.php';
 
 $request = "SELECT 
                 c.idClient,
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ switch ($request) {
     case preg_match('(fic\?q=\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$)', $request) == 1 :
         $guid = $_GET['q'];
         $_SESSION["guid"] = $guid;
-        $_SESSION['client']= TRUE;
+        $_SESSION['client'] = TRUE;
         $sqlRequest = $db->prepare("SELECT GUID.guid as guidInit, clients.guid as guidClient, GUID.isSociete FROM clients_guid GUID LEFT JOIN clients ON clients.guid = GUID.guid WHERE GUID.guid = UPPER(:guid)"); // tout faire en une requete
         $sqlRequest->execute([":guid" => $guid]);
         $resultat = $sqlRequest->fetch();
@@ -15,7 +15,7 @@ switch ($request) {
 
         if ((isset($resultat["guidInit"])) && $resultat["guidClient"] == null) {
             $_SESSION["isSociete"] = $resultat["isSociete"] == 1 ? true : false;
-            header( 'Location: accueil.php');
+            header('Location: accueil.php');
             exit();
 
         } else if ((isset($resultat["guidInit"])) && (isset($resultat["guidClient"])) && $resultat["guidInit"] == $resultat["guidClient"]) {
@@ -26,6 +26,16 @@ switch ($request) {
             header('Location: brokenLink.php');
             exit();
         }
+        break;
+    case $request === '/exporterXml' :
+        $_SESSION['client'] = TRUE;
+        header('Location:exporterXml.php');
+        exit();
+        break;
+    case $request === '/exporterXmlComplet' :
+        $_SESSION['client'] = TRUE;
+        header('Location:exporterXmlComplet.php');
+        exit();
         break;
 
     default:
